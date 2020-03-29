044eae396a4cfb55f1227cdaec0d6be0becd391c
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2020-03-29 17:07:55 +0200

    schowanie extension function do klasy w trzecim

--- maze/src/tabu/Path.kt
+++ maze/src/tabu/Path.kt
@@ -3,12 +3,12 @@ package tabu
 import tabu.enums.Moves
 import kotlin.random.Random
 
-fun <T> Array<T>.swap(a: Int, b: Int): Array<T> = this.also {
-    it[a] = this[b]
-    it[b] = this[a]
-}
-
 class Path(val sequence: MutableList<Moves> = mutableListOf()) {
+    private fun <T> Array<T>.swap(a: Int, b: Int): Array<T> = this.also {
+        it[a] = this[b]
+        it[b] = this[a]
+    }
+
     fun append(move: Moves): Path {
         sequence.add(move)
         return this
@@ -16,14 +16,12 @@ class Path(val sequence: MutableList<Moves> = mutableListOf()) {
 
     private fun transpose(i: Int, j: Int): Path = Path(mutableListOf(*sequence.toTypedArray().swap(i, j)))
 
-
     private fun inverse(from: Int, to: Int): Path = Path(
         (sequence.slice(0 until from) +
                 sequence.slice(from..to).reversed() +
                 sequence.slice(to + 1 until sequence.size))
             .toMutableList()
     )
-
 
     fun randomTweak(): Path {
         val i = Random.nextInt(sequence.size)
